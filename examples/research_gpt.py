f6b7899665ad24d6403c4475d63507efeca8b575
FayazRahman <fayazrahman4u@gmail.com>
2023-04-25 03:30:20 +0530

    examples

--- examples/research_gpt.py
+++ examples/research_gpt.py
@@ -8,9 +8,9 @@ agent.description = "an AI assistant that researches and finds the best tech pro
 
 agent.goals = [
     "Search for the best headphones on Google",
-    "Analyze specs, prices and reviews to find the top 5 best headphones",
     "Write the list of the top 5 best headphones and their prices to a file",
     "Summarize the pros and cons of each headphone and write it to a different file called 'summary.txt'",
+    "There will be no user assistance. Terminate once writing both files is complete."
 ]
 
 agent.cli()
