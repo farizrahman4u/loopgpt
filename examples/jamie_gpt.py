d37114bcd7f15a73f30c7498e337c823704fdf3d
FayazRahman <fayazrahman4u@gmail.com>
2023-06-04 07:50:59 +0530

    upd

--- examples/jamie_gpt.py
+++ examples/jamie_gpt.py
@@ -1,20 +1,7 @@
 import loopgpt
 from loopgpt.tools import GoogleSearch, Browser, WriteToFile, AppendToFile
-from loopgpt.models import AzureOpenAIModel
-from loopgpt.embeddings import AzureOpenAIEmbeddingProvider
 
-import openai
-import os
-
-openai.api_type = "azure"
-openai.api_base = "https://loopgpt-azure-openai.openai.azure.com/"
-openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv("AZURE_OPENAI_KEY")
-
-model = AzureOpenAIModel("loop-gpt-35-turbo")
-emb = AzureOpenAIEmbeddingProvider("loop-text-embedding-ada-002")
-
-agent = loopgpt.Agent(tools=[GoogleSearch, Browser, WriteToFile, AppendToFile], model=model, embedding_provider=emb, temperature=0.2)
+agent = loopgpt.Agent(tools=[GoogleSearch, Browser, WriteToFile, AppendToFile], temperature=0.2)
 
 agent.name = "Jamie"
 
@@ -26,8 +13,8 @@ agent.description = (
 )
 
 agent.goals = [
-    "Research events of the past week on the internet",
-    "Write a podcast outline for 3 guests on 5 topics with descriptions, references and targeted questions for the host (Joe Rogan)",
+    "Research relevant events of the past week for all the guests on the internet.",
+    "Write a file for each guest with 5 topics to discuss with long descriptions, website links and targeted questions for the host (Joe Rogan)."
     "Terminate the session once all 3 files are completed.",
 ]
 
