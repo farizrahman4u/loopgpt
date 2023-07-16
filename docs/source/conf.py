7e1848c6037c868961cccc712da00a8f6195e9a7
FayazRahman <fayazrahman4u@gmail.com>
2023-07-17 05:09:18 +0530

    update doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "LoopGPT"
 copyright = "2023, Fariz Rahman, Fayaz Rahman"
 author = "Fariz Rahman, Fayaz Rahman"
-release = "0.0.16"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
